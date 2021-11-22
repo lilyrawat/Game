a502cde23e2e7ce403e68ab669b0da0b91f3a9bc
lilyrawat <lilyrawat2001@gmail.com>
2021-11-22 13:48:16 +0530

    Level 4

--- Game Design/Assets/Scripts/Menu/PauseMenu.cs
+++ Game Design/Assets/Scripts/Menu/PauseMenu.cs
@@ -38,7 +38,7 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void Restart(){
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void QuitGame(){
